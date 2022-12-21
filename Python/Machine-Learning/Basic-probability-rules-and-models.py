358206bdf9b082bcee005bbf9ee67f9a71246dbf
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2022-12-21 20:25:39 +0530

    Update Basic-probability-rules-and-models.py

--- Python/Machine-Learning/Basic-probability-rules-and-models.py
+++ Python/Machine-Learning/Basic-probability-rules-and-models.py
@@ -1,3 +1,5 @@
+# https://www.hackerearth.com/practice/machine-learning/prerequisites-of-machine-learning/basic-probability-models-and-rules/tutorial/
+
 m,a,r = float(input()),float(input()),float(input())
 p = r *(a * (1 - m) + m * (1 - a))
 print('%.6f' % p)
