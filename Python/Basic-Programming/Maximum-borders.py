1d957e93177a1bad629d25e970f156154b682e8c
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2022-12-21 20:27:19 +0530

    Update Maximum-borders.py

--- Python/Basic-Programming/Maximum-borders.py
+++ Python/Basic-Programming/Maximum-borders.py
@@ -1,3 +1,5 @@
+# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/maximum-border-9767e14c/
+
 no = int(input())
 for n in range(no):
     i,t = [int(x) for x in input().strip().split()],0
