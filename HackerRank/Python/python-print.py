53416845b162a6032f106d5302b67f4306fdcf61
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-02 20:10:46 +0530

    Update python-print.py

--- HackerRank/Python/python-print.py
+++ HackerRank/Python/python-print.py
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/python-print
 n = int(input())
-    s = ''
-    for i in range(1,n+1):
-        s+= str(i)
-    print(s)
+s = ''
+for i in range(1,n+1):
+    s+= str(i)
+print(s)
