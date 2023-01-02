adc48ff689aee72c64148875e0f9106584bbd898
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-02 21:09:07 +0530

    Delete Basic-probability-rules-and-models.py

--- HackerEarth/Machine-Learning/Basic-probability-rules-and-models.py
+++
@@ -1,5 +0,0 @@
-# https://www.hackerearth.com/practice/machine-learning/prerequisites-of-machine-learning/basic-probability-models-and-rules/tutorial/
-
-m,a,r = float(input()),float(input()),float(input())
-p = r *(a * (1 - m) + m * (1 - a))
-print('%.6f' % p)
