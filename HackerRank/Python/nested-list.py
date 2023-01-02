3e22c7d56d2701dbcf078562f8c84ac93706a4c5
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-02 20:19:53 +0530

    Update nested-list.py

--- HackerRank/Python/nested-list.py
+++ HackerRank/Python/nested-list.py
@@ -1,10 +1,10 @@
 # https://www.hackerrank.com/challenges/nested-list
 students_grade = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        students_grade += [[name,score]]
-    s = sorted(list(set([students_grade[i][1] for i in range(0,len(students_grade))])))
-    low = [[students_grade[i][0]] for i in range(0,len(students_grade)) if students_grade[i][1] == s[1]]
-    low = sorted(low)
-    [print(y[0]) for y in low]
+for _ in range(int(input())):
+name = input()
+score = float(input())
+students_grade += [[name,score]]
+s = sorted(list(set([students_grade[i][1] for i in range(0,len(students_grade))])))
+low = [[students_grade[i][0]] for i in range(0,len(students_grade)) if students_grade[i][1] == s[1]]
+low = sorted(low)
+[print(y[0]) for y in low]
