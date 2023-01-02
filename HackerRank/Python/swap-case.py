09163e173ff0a02c1a5d0ef2288101b9781ee064
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-02 22:02:58 +0530

    Create swap-case.py

---
+++ HackerRank/Python/swap-case.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/swap-case
+
+def swap_case(s):
+    ss = ''
+    for i in s:
+        if i.isupper():
+            ss+= i.lower()
+        elif i.islower():
+            ss+= i.upper()
+        else:
+            ss+= i
+    return ss
