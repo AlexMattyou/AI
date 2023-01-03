93bc6cb59bc653520af6d55394e4c4d2a4f306be
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-03 20:50:52 +0530

    Delete HackerEarth/Codemonk directory

--- HackerEarth/Codemonk/Monk-and-Rotation.py
+++
@@ -1,9 +0,0 @@
-# https://www.hackerearth.com/practice/codemonk/
-
-for c in range(int(input())):
-    i = [int(x) for x in input().split()]
-    v = [int(x) for x in input().split()]
-    g = i[1]%i[0]
-    v = v[-g:]+v[:-g]
-    [print(p,end=' ')for p in v]
-    print()
