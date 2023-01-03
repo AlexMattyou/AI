32dbda9a4eb0da3bf8f5e6d0c5a469b1e370043e
Alex Matt <98243103+AlexMattyou@users.noreply.github.com>
2023-01-03 20:50:41 +0530

    Delete HackerEarth/Basic-Programming directory

--- HackerEarth/Basic-Programming/Ali-and-Helping-innocent-people.py
+++
@@ -1,8 +0,0 @@
-# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/cartag-948c2b02/
-
-i = input().strip()
-a = 'invalid'
-if i[2] not in ["A","E","I","O","U","Y"]:
-    if (int(i[0])+int(i[1]))%2==0 and (int(i[3])+int(i[4]))%2==0 and (int(i[4])+int(i[5]))%2==0 and (int(i[7])+int(i[8]))%2==0:
-        a = 'valid'
-print(a)
--- HackerEarth/Basic-Programming/Factorial!.py
+++
@@ -1,7 +0,0 @@
-# https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-factorial/
-
-x = int(input())
-f = 1
-for i in range(1,x+1):
-    f *= i
-print(f)
